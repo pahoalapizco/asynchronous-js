97bf3299502d3adef877603679b20dc0767d42f4
Paola Alapizco <pahoc.alapizco@gmail.com>
2020-01-02 12:52:31 -0700

    Project structure

---
+++ src/utils/globals.js
@@ -0,0 +1,15 @@
+exports.readyState = {
+  REQUEST_NO_INI: 0,
+  CONNECTION_ESTAB: 1,
+  RECEIVED: 2,
+  PROCESSING: 3,
+  FINISHED: 4
+}
+
+exports.status = {
+  OK: 200,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404
+}
+
+exports.API = 'https://rickandmortyapi.com/api/character/'
