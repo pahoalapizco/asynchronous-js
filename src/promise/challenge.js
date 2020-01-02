ffef382c160de26ecedcf2552b8820de9c0e3c62
Paola Alapizco <pahoc.alapizco@gmail.com>
2020-01-02 14:15:27 -0700

    Promises examples

--- src/promise/challenge.js
+++ src/promise/challenge.js
@@ -1,1 +1,36 @@
 'use strict'
+
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
+const { readyState, status, API } = require('../utils/globals')
+
+const fetchData = url_api => {
+  return new Promise((resolve, reject) => {
+    const xhttp = new XMLHttpRequest()
+    // Method, URL, async 
+    xhttp.open('GET', url_api, true) 
+    xhttp.onreadystatechange = (() => {
+      if (xhttp.readyState === readyState.FINISHED) {
+
+        xhttp.status === status.OK
+          ? resolve(JSON.parse(xhttp.responseText))
+          : reject(new Error(`Error con ${url_api}`))
+      }
+    })
+    xhttp.send()
+  })
+}
+
+
+fetchData(API)
+  .then(data => {
+    console.log(data.info.count)
+    return fetchData(`${API}${data.results[0].id}`)
+  })
+  .then(data => {
+    console.log(data.name)
+    return fetchData(data.origin.url)
+  })
+  .then(data => {
+    console.log(data.dimension)
+  })
+  .catch(error => console.log(error))
