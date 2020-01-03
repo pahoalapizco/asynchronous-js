08b716c5c6eeb72767edd4f66e0fb3d279557597
Paola Alapizco <pahoc.alapizco@gmail.com>
2020-01-03 10:38:15 -0700

    async-await examples

--- src/async-await/challenge.js
+++ src/async-await/challenge.js
@@ -1,1 +1,40 @@
 'use strict'
+
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
+const { readyState, status, API } = require('../utils/globals')
+
+const fetchData = url_api => {
+  return new Promise((resolve, reject) => {
+    const xhttp = new XMLHttpRequest()
+    // Method, URL, async 
+    xhttp.open('GET', url_api, true) 
+    xhttp.onreadystatechange = (() => {
+      if (xhttp.readyState === readyState.FINISHED) {
+
+        xhttp.status === status.OK
+          ? resolve(JSON.parse(xhttp.responseText))
+          : reject(new Error(`Error con ${url_api}`))
+      }
+    })
+    xhttp.send()
+  })
+}
+
+const getInfo = async (url_api) => {
+  try {
+    const data = await fetchData(url_api)    
+    const character = await fetchData(`${url_api}${data.results[0].id}`)    
+    const origin = await fetchData(character.origin.url)  
+    
+    console.log(`Total characters: ${data.info.count}`)
+    console.log(`Character: ${character.name}`)
+    console.log(`Dimension: ${origin.dimension}`)
+
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+console.log('Before')
+getInfo(API)
+console.log('After')
--- src/async-await/index.js
+++ src/async-await/index.js
@@ -1,1 +1,31 @@
 'use strict'
+
+const doSomethingAsync = (value) => {
+  return new Promise ((resolve, reject) => {
+    (value)
+      ? setTimeout(() => { resolve('Doing something') }, 3000)
+      : reject(new Error('whooops! :c'))
+  })
+}
+
+const doSomething = async (value) => {
+  const something = await doSomethingAsync(value)
+  console.log(something)
+}
+
+console.log('Before')
+doSomething(true)
+console.log('After')
+
+const anotherFuntion = async (value) => {
+  try {
+    const something = await doSomethingAsync(value)
+    console.log(something)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+console.log('Before 2')
+anotherFuntion(false)
+console.log('After 3')
